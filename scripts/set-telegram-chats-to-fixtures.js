66b51c7984de6198ac3fc0f5a66e8bfacc22ff4f
ryzhak <ryzhak.vladimir@gmail.com>
2019-04-26 01:03:18 +0300

    fix: settings tg group names

--- scripts/set-telegram-chats-to-fixtures.js
+++ scripts/set-telegram-chats-to-fixtures.js
@@ -17,9 +17,8 @@ async function main() {
 		const fixtures = await Fixture.find();
 		const groupLink = require(`${__dirname}/../dumps/telegram_group_link_13042019.json`);
 		// set telegram chats for fixtures
-		for(let fixtureId of Object.keys(fixtures)) {
-			const fixture = fixtures[fixtureId];
-			await Fixture.findOneAndUpdate({id: fixtureId}, {telegram_invite_link: groupLink[fixture.telegram_group_name]}, {upsert: true}).catch((err) => { console.log(err); });
+		for(let fixture of fixtures) {
+			await Fixture.findOneAndUpdate({id: fixture.id}, {telegram_invite_link: groupLink[fixture.telegram_group_name]}).catch((err) => { console.log(err); });
 		}
 		console.log('finished');
 		process.exit();
