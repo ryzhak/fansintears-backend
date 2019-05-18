df3fa139134afe4ed9eec2139dbfa5c165eb4fff
ryzhak <ryzhak.vladimir@gmail.com>
2019-05-18 15:38:10 +0300

    feat: add media scraper basic examples

---
+++ scripts/scrape-media.js
@@ -0,0 +1,22 @@
+const userInstagram = require('user-instagram');
+const scrapeTwitter = require('scrape-twitter');
+
+// let tweets = [];
+// new scrapeTwitter.TweetStream('from:TrollFootball', 'latest', {count: 1})
+// .on('data', (tweet) => { tweets.push(tweet); })
+// .on('end', () => {
+// 	console.log(tweets);	
+// })
+// .on('error', (err) => { reject(err); });
+
+// userInstagram('https://www.instagram.com/footballmemesinsta')
+// .then(data => {
+// 	console.log(data.posts.length);
+//   	console.log(`Full name is: ${data.fullName}`);
+// })
+// .catch(e => {
+// 	// Error will trigger if the account link provided is false.
+//   	console.error(data);
+// });
+
+console.log('here');
