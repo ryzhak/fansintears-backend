9e8572759a52ad95e37c2bda18e5a0c5ef4aee25
ryzhak <ryzhak.vladimir@gmail.com>
2019-05-25 12:27:29 +0300

    feat: add players scraper

--- scripts/scrape-media.js
+++ scripts/scrape-media.js
@@ -11,15 +11,18 @@ main();
  */
 async function main() {
 	let mediaPosts = [];
-	// for all accounts
-	for(let mediaSource of mediaSources.memes) {
-		// get latest posts
-		let posts = [];
-		if(mediaSource.source === 'twitter') posts = await twitter.getPostsByAccount(mediaSource.uri);
-		if(mediaSource.source === 'instagram') posts = await instagram.getPostsByAccount(mediaSource.uri);
-		// convert posts to FansInTears format
-		posts = posts.map(post => socialMediaConverter.convertPost(post, mediaSource.source, mediaSource.uri));
-		mediaPosts.push(...posts);
+	// for all media post groups
+	for(let group of Object.keys(mediaSources)) {
+		// for all accounts
+		for(let mediaSource of mediaSources[group]) {
+			// get latest posts
+			let posts = [];
+			if(mediaSource.source === 'twitter') posts = await twitter.getPostsByAccount(mediaSource.uri);
+			if(mediaSource.source === 'instagram') posts = await instagram.getPostsByAccount(mediaSource.uri);
+			// convert posts to FansInTears format
+			posts = posts.map(post => socialMediaConverter.convertPost(post, mediaSource.source, mediaSource.uri, group));
+			mediaPosts.push(...posts);
+		}	
 	}
 	console.log(mediaPosts);
 	console.log(mediaPosts.length);
